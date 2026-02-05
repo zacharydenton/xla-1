485af25f75792f1118c34c5c8874250c45e2da71
xla authors <google-ml-automation@google.com>
2026-02-05 05:56:42 -0800

    Move symbolic map serialization tests to a dedicated file.
    This change refactors the tests for parsing and printing SymbolicExpr and SymbolicMap from symbolic_expr_test.cc and symbolic_map_test.cc into a new test file, symbolic_map_serialization_test.cc. I missed to move the tests when I moved the methods, my bad.
    
    PiperOrigin-RevId: 865904704

--- xla/hlo/analysis/symbolic_map.h
+++ xla/hlo/analysis/symbolic_map.h
@@ -97,6 +97,12 @@ class SymbolicMap {
   // Returns true if all result expressions are constant.
   bool IsConstant() const;
 
+  // Returns true if any result expression depends on the given dimension.
+  bool IsFunctionOfDim(int64_t dim_id) const;
+
+  // Returns true if any result expression depends on the given symbol.
+  bool IsFunctionOfSymbol(int64_t symbol_id) const;
+
   // Returns a vector containing the values of all the results. CHECK-fails if
   // any result expression is not a constant.
   llvm::SmallVector<int64_t> GetConstantResults() const;
@@ -178,10 +184,6 @@ SymbolicMap CompressDims(const SymbolicMap& map,
 // Expressions are updated to use the new symbol indices.
 SymbolicMap CompressSymbols(const SymbolicMap& map,
                             const llvm::SmallBitVector& unused_symbols);
-
-// Parses a SymbolicMap from its string representation.
-SymbolicMap ParseSymbolicMap(absl::string_view serialized_symbolic_map,
-                             mlir::MLIRContext* mlir_context);
 
 template <typename H>
 H AbslHashValue(H h, const llvm::SmallVector<SymbolicExpr>& vec) {
